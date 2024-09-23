e67c89f042691ba8ac9536d69e905510d2071d22
Betsabe Navarro <betsabenavarro@Betsabes-MacBook-Pro.local>
2024-09-23 15:13:19 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-ad0058be-4bcf-44dc-ac94-ecbc30a507df.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
